91870aebe6fd17b34a039ea152937391fb191423
Michael <57787676+baentsch@users.noreply.github.com>
2021-03-10 08:42:32 +0100

    addressing review comments [skip-ci]

--- oqs_scripts/testrun.py
+++ oqs_scripts/testrun.py
@@ -3,11 +3,16 @@ import sys
 import subprocess
 import os
 import time
+import shutil
+import tempfile
+import urllib.request
 
-# Parameter checks already done in shellscript
+with urllib.request.urlopen('https://test.openquantumsafe.org/CA.crt') as response:
+    with tempfile.NamedTemporaryFile(delete=False) as ca_file:
+        shutil.copyfileobj(response, ca_file)
 
-with open("assignments.json", "r") as f:
-   jsoncontents = f.read();
+with urllib.request.urlopen('https://test.openquantumsafe.org/assignments.json') as response:
+   jsoncontents = response.read()
 
 onlysigoutput = False
 if len(sys.argv)>1:
@@ -18,10 +23,16 @@ for sig in assignments:
     print("Testing %s:" % (sig))
     for kem in assignments[sig]:
        # assemble testing command
-       cmd = "(echo \'GET /\'; sleep 0.2) | ../build/tool/bssl client -connect test.openquantumsafe.org:"+str(assignments[sig][kem]) + " -root-certs CA.crt 2>&1"
+       cmd = "(echo \'GET /\'; sleep 0.2) | build/tool/bssl client -connect test.openquantumsafe.org:"+str(assignments[sig][kem]) + " -root-certs "+ca_file.name+" 2>&1"
        if kem!="*": # don't prescribe KEM
           cmd=cmd+" -curves "+kem
        output = os.popen(cmd).read()
+#       proc = subprocess.Popen([os.path.join("build", "tool", "bssl"), "client", 
+#                                     "-connect", "test.openquantumsafe.org:"+str(assignments[sig][kem]),
+#                                     "-curves", kem,
+#                                     "-root-certs",  ca_file.name])
+#       time.sleep(1)
+#       output, stderr = proc.communicate(input="GET /\n\n")
        if not ("Successfully" in output):
            print("Error with command '%s': \n%s\n" % (cmd, output))
            if (not onlysigoutput):
